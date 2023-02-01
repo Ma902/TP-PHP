6151f963adc5d5a1b336fcdf36f76f3649978fb1
Ma902 <65480644+Ma902@users.noreply.github.com>
2023-02-01 10:36:17 +0100

    add tp4 and tp5

---
+++ tp5.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TP5</title>
+</head>
+<body>
+    <?php
+            $a = 0;
+            $b = 0;
+            $a = $_POST['prenom'];
+            $b = $_POST['nom'];
+            echo $a+$b."<br/>";
+            echo $a-$b."<br/>";
+            echo $a*$b."<br/>";
+            echo $a/$b."<br/>";
+
+
+
+
+
+
+    ?>
+</body>
+</html>
